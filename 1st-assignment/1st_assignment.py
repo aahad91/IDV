22b2aa3ef335768125004d77c49ed5498bca34e3
aahad91 <abdulahadayaz@hotmail.com>
2020-04-26 18:50:01 +0200

    added colorbar and labeling

--- 1st-assignment/1st_assignment.py
+++ 1st-assignment/1st_assignment.py
@@ -1,25 +1,48 @@
+"""
+This program shows water flowing through a channel. Winds acting upon
+the (open) surface of the water create turbulences inside the water. Movements
+of water particles (caused by winds) were calculated by students in the first
+supercomputing class by Prof. Lloyd Fosdick, University of Colorado, 1992.
+File “field2.irreg” contains data describing the particle movement in 2d slide
+perpendicular to the length of the channel. Data is given for a regular 82x82
+grid in following format:starting position(x,y,z) and relative movement(u,v,w).
+
+Name: Abdul Ahad Ayaz
+"""
+# Used Packages
 import pandas as pd
 import numpy as np
-import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
-# data cleaning
+
+# Data Cleaning
 da = pd.read_csv('field2.irreg')
 db = da.drop([0, 1, 2, 3, 4])
+
+# Data formatting
 db.columns = ['Data']
 db[['x', 'y', 'z', 'u', 'v', 'w']] = db.Data.str.split(expand=True)
 db.drop(columns=['Data'], inplace=True)
 db.to_csv('test.csv')
-# print(db.shape)
-xc = pd.read_csv('test.csv')
-# calculating the displacement
-flow = plt.subplots(figsize=(10, 10))
-for index, row in xc.iterrows():
-    a = row.x + row.u
-    b = row.y + row.v
-    c = row.x - a
-    d = row.y - b
-    col = np.hypot(row.u, row.v)
-    flow = plt.quiver(row.x, row.y, row.u, row.v, col, cmap='jet', norm=colors.Normalize(vmin=0, vmax=1), scale=4, width=0.001, headlength=10, headwidth=10)
-    #ax.arrow(row.x, row.y, row.u, row.v, fc='white', ec='blue', head_length=0.009, head_width=0.007)
+
+# Plotting Datapoints
+flow_data = pd.read_csv('test.csv')
+flow_plt = plt.subplots(dpi=200)
+normc = colors.Normalize(vmin=0, vmax=1)
+for index, row in flow_data.iterrows():     # looping throught rows of data
+    clr = np.hypot(row.u, row.v)            # calculating color value
+    flow_plt = plt.quiver(row.x, row.y, row.u, row.v, clr, cmap='jet',
+                        norm=normc, scale=5, width=0.001, headlength=8,
+                        headwidth=8, pivot='mid')
+
+# Colorbar setting
+clb = plt.colorbar(flow_plt, ticks=[0, 0.5, 1])
+clb.set_ticklabels(['Low', 'Medium', 'High'])
+clb.set_label('Flow Velocity', rotation=90)
+
+# Plot labeling
+plt.title('Vector Field Visualization')
+plt.xlabel('X Equilavent of Vectors')
+plt.ylabel('Y Equilavent of Vectors')
+plt.savefig('vector_field.jpeg', dpi=200)
 plt.show()

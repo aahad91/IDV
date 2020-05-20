87cd25ea27cc4608899e914734972cc0298fd028
aahad91 <abdulahadayaz@hotmail.com>
2020-05-20 02:48:06 +0200

    ploting remaining

--- 2nd-assignment/2nd-assignment.py
+++ 2nd-assignment/2nd-assignment.py
@@ -20,32 +20,34 @@ import matplotlib.pyplot as plt
 # ---Task Functions--- #
 
 
+# Task A
 def profile_line(data):
     array2d = data.reshape(512, 512)
     prof_line = []
     for i in range(0, 512):
-        prof_line.append(array2d[i, 255])
-    plt.plot(prof_line)
-    plt.title('256 Profile Line')
-    plt.show()
+        prof_line.append(array2d[255, i])
+    return prof_line
 
 
+# Task B
 def mean1(data):
     mean = sum(data)/len(data)
     print(mean)
 
 
+# Task B
 def variance1(data):
     var = np.var(data)
     print(var)
 
 
+# Task C
 def plot_histogram(data):
-
-    plt.hist(data, histtype='step')
+    plt.hist(data, bins=255)
     plt.show()
 
 
+# Task D
 def linear_transform(data):
     i_max = 0
     i_min = 0
@@ -59,24 +61,44 @@ def linear_transform(data):
         linear_tf[x] = ((i - i_min) / (i_max - i_min))*255
     linear_tf = np.asarray(linear_tf)
     pixel = linear_tf.reshape(512, 512)
-    plt.imshow(pixel)
-    plt.show()
+    return pixel
 
 
-
+# Task E
 def nonlinear_transform(data):
-    nlinear_tf =[None]*len(data)
+    nlinear_tf = [None]*len(data)
     for y in range(0, len(data)):
         px = data[y]
         nlinear_tf[y] = math.log2(px + 1)
     nlinear_tf = np.asarray(nlinear_tf)
     pixel = nlinear_tf.reshape(512, 512)
-    plt.imshow(pixel, 'gray')
-    plt.show()
+    return pixel
 
-# def boxcar_filter():
 
-# def median_filter():
+# Task F
+def boxcar_filter(data):
+    filter = 11
+    pad = filter // 2
+    data = data.reshape(512, 512)
+    row, col = data.shape
+    for x in range(pad, row-pad):
+        for y in range(pad, col-pad):
+            filter_val = np.sum(data[x:filter+x, y:filter+y])*(1/121)
+            data[x+5, y+5] = filter_val
+    return data
+
+
+# Task G
+def median_filter(data):
+    filter = 11
+    pad = filter // 2
+    data = data.reshape(512, 512)
+    row, col = data.shape
+    for x in range(pad, row-pad):
+        for y in range(pad, col-pad):
+            filter_val = np.sort(data[x:filter+x, y:filter+y])
+            data[x+5, y+5] = filter_val[pad, pad]
+    return data
 
 # def visual():
 
@@ -88,10 +110,12 @@ if __name__ == "__main__":
     mean1(raw)
     variance1(raw)
     #profile_line(raw)
-    #plt_histogram(raw)
+    #plot_histogram(raw)
     #linear_transform(raw)
     #print(len(raw))
     #print(array2d)
-    nonlinear_transform(raw)
+    #nonlinear_transform(raw)
     #test(raw)
-    #print(type(raw))
+    #print(np.shape(raw))
+    #boxcar_filter(raw)
+    #median_filter(raw)

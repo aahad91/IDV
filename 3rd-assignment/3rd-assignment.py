f3ac423d2c25ba7c7bc811d00aa5ccbe0d278b39
aahad91 <abdulahadayaz@hotmail.com>
2020-05-20 23:22:56 +0200

    folder structured for 3ed assignement

---
+++ 3rd-assignment/3rd-assignment.py
@@ -0,0 +1,15 @@
+"""
+
+Name: Abdul Ahad Ayaz
+"""
+# ---packages--- #
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+
+# ---Task Functions--- #
+
+# ----------------------------------#
+
+if __name__ == "__main__":

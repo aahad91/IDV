f76a3f9ef7c32848cdd0f429b22eb9991e3daa06
aahad91 <abdulahadayaz@hotmail.com>
2020-05-28 19:07:39 +0200

    added function for histogram_equalization

--- 3rd-assignment/3rd-assignment.py
+++ 3rd-assignment/3rd-assignment.py
@@ -32,11 +32,54 @@ def prof_line_max(data):
     for i in range(0, 500):
         prof_line.append(data[pline[0], i])
     plt.plot(prof_line)
+    # plt.show()
+    return prof_line
+# def histo_line(data):
+
+
+def rescale_val(data):
+    # i_max = max_val(data)
+    # i_min = min_val(data)
+    tf = np.zeros([len(data), len(data)])
+    for x in range(0, len(data)):
+        for y in range(0, len(data)):
+            i = data[x][y]
+            tf[x][y] = math.log2(i + 1)
+            # tf[x][y] = (((i - i_min) / (i_max - i_min))*255)
+    print(np.shape(tf))
+    plt.imshow(tf, cmap='gray')
     plt.show()
-    print(pline[0])
-# def histo_line():
-# def rescale_val():
-# def histo_eq():
+    return tf
+
+
+def histo_eq(data):
+    maxval = max_val(data)
+    val = {}
+    cdf = {}
+    sum = 0
+    h_equ = np.ones([len(data), len(data)])
+    for x in range(0, len(data)):
+        for y in range(0, len(data)):
+            i = data[x][y]
+            if i in val:
+                val[i] += 1
+            else:
+                val[i] = 1
+    for q, p in sorted(val.items()):
+        pr = p/(500*500)
+        sum += pr
+        cdf[q] = round(sum*maxval)
+    for x in range(0, len(data)):
+        for y in range(0, len(data)):
+            v = data[x][y]
+            for key, value in cdf.items():
+                if v == key:
+                    h_equ[x][y] = value
+    print(h_equ)
+    plt.imshow(h_equ, 'gray')
+    plt.show()
+
+
 # def color_transform():
 # ----------------------------------#
 
@@ -48,11 +91,12 @@ if __name__ == "__main__":
     band2 = np.flip(df, 0)
     print(df)
     print('\n')
-    print(band2)
-    #plt.imshow(band2, cmap='gray')
-    #plt.show()
+    # plt.imshow(band2, cmap='gray')
+    # plt.show()
     print("Max Value:", max_val(band2))
     print("Min Value:", min_val(band2))
     print("Mean:", mean_val(band2))
     print("Variance:", var_val(band2))
-    prof_line_max(band2)
+    # prof_line_max(band2)
+    # rescale_val(band2)
+    histo_eq2(band2)

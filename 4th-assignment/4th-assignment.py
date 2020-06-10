b6325eb24cf5bfa91af0727ead79750a0e7bea6c
aahad91 <abdulahadayaz@hotmail.com>
2020-06-10 18:01:41 +0200

    fig polishing!!

--- 4th-assignment/4th-assignment.py
+++ 4th-assignment/4th-assignment.py
@@ -9,29 +9,40 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 
-# ----------------------------------#
 
 
-if __name__ == "__main__":
-
-    # --- Reading Data--- #
-    data = pd.read_csv('DataWeierstrass.csv', delimiter=';')
-    
-    # ---Scatter Matrix--- #
+# --- Task Functions--- #
+def scatter_matrix(data):
     sm = sns.pairplot(data, hue='professor', kind='scatter', diag_kind='None', height=2, aspect=2, markers='+')
     for ax in sm.axes.flat:
         plt.setp(ax.get_xticklabels(), rotation=0)
+    plt.subplots_adjust(top=0.95)
+    sm.fig.suptitle('Best Teacher Visualization: Scatter Matrix', fontsize=16)
+    plt.savefig('scatter_matrix.jpeg', dpi=200)
 
-    # ---Parallel Cooedinates--- #
+
+def parallel_coordinate(data):
     u, data['professor1'] = np.unique(data['professor'], return_inverse=True)
     v, data['lecture1'] = np.unique(data['lecture'], return_inverse=True)
     data1 = data.drop(['professor', 'lecture'], axis=1)
     for i in data1.index:
         data1.at[i, 'professor1'] += 1
         data1.at[i, 'lecture1'] += 1
+
     pc = px.parallel_coordinates(data1[['professor1', 'lecture1', 'participants', 'professional expertise',
                                         'motivation', 'clear presentation', 'overall impression']], color='professor1',
                                         labels={'professor1': 'professor', 'lecture1': 'lecture'},
-                                        color_continuous_scale=px.colors.diverging.Tealrose, color_continuous_midpoint=2)
-    pc.show()
+                                        color_continuous_scale=px.colors.cyclical.mrybm,
+                                        title='Best Teacher Visualization: Parallel Coordinates')
+    return pc
+
+
+# ----------------------------------#
+if __name__ == "__main__":
+
+    # --- Reading Data--- #
+    data = pd.read_csv('DataWeierstrass.csv', delimiter=';')
+
+    scatter_matrix(data)
+    parallel_coordinate(data).show()
     plt.show()
---
+++ 4th-assignment/README.md
@@ -0,0 +1,6 @@
+## 4th Assignment - Multi-Variate Data, Scatter Plot, Parallel Coordinates
+
+#### Scatter Matrix:
+![Scatter Matrix](scatter_matrix.jpeg)
+#### Parallel Coordinates:
+![Parallel Coordinates](parallel_coordinate.png)
